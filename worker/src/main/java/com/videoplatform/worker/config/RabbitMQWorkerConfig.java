7a6d7e5a489bcc55b6e5b51b180805b1f0d61029
Arpit Sharma <arpitsharma@Arpits-MacBook-Air.local>
2026-06-29 13:59:36 +0530

    fix: map RabbitMQ TypeId header to resolve ClassNotFoundException during deserialization

--- worker/src/main/java/com/videoplatform/worker/config/RabbitMQWorkerConfig.java
+++ worker/src/main/java/com/videoplatform/worker/config/RabbitMQWorkerConfig.java
@@ -1,15 +1,30 @@
 package com.videoplatform.worker.config;
 
+import com.videoplatform.worker.dto.TranscodeMessage;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class RabbitMQWorkerConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        
+        DefaultJackson2JavaTypeMapper typeMapper = new DefaultJackson2JavaTypeMapper();
+        typeMapper.setTrustedPackages("*");
+        
+        Map<String, Class<?>> idClassMapping = new HashMap<>();
+        idClassMapping.put("com.videoplatform.api.dto.TranscodeMessage", TranscodeMessage.class);
+        typeMapper.setIdClassMapping(idClassMapping);
+        
+        converter.setJavaTypeMapper(typeMapper);
+        return converter;
     }
 }
